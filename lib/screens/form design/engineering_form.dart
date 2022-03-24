62bc88449576d0af410819e74c0e061f3fcb503c
kawsar07 <kawsar.ali.cse@gmail.com>
2022-03-24 18:26:03 +0600

    Home Page

--- lib/screens/form design/engineering_form.dart
+++ lib/screens/form design/engineering_form.dart
@@ -7,6 +7,7 @@ import 'package:rcc/widgets/custom_banner.dart';
 import 'package:rcc/widgets/custom_dropdown.dart';
 import 'package:rcc/widgets/custom_text_field.dart';
 import 'package:rcc/widgets/gradient_text.dart';
+
 class EngineeringFormPage extends StatefulWidget {
   const EngineeringFormPage({Key? key}) : super(key: key);
 
@@ -22,78 +23,75 @@ class _EngineeringFormPageState extends State<EngineeringFormPage> {
     "4",
     "5",
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: NewGradientAppBar(
-            automaticallyImplyLeading: false,
-            titleSpacing: 3.0,
-            elevation: 15.0,
-            leading: const BackButton(),
-            title: const Text('প্রকৌশল বিভাগ'),
-            actions: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.only(right: 14.0),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: const Icon(
-                      MdiIcons.bellRing,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                  )),
-            ],
-            gradient:
-            LinearGradient(colors: [Palette.mcgrcc, HexColor("#FB9203")])),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(
-                height: 8.0,
-              ),
-              Container(
-                margin: const EdgeInsets.all(10.0),
-                child: const GradientText(
-                  "জল এবং গ্যাস সংযোগ পরিষেবা",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+      appBar: NewGradientAppBar(
+          automaticallyImplyLeading: false,
+          titleSpacing: 3.0,
+          elevation: 15.0,
+          leading: const BackButton(),
+          title: const Text('প্রকৌশল বিভাগ'),
+          actions: <Widget>[
+            Padding(
+                padding: const EdgeInsets.only(right: 14.0),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: const Icon(
+                    MdiIcons.bellRing,
+                    color: Colors.white,
+                    size: 24.0,
                   ),
+                )),
+          ],
+          gradient:
+              LinearGradient(colors: [Palette.mcgrcc, HexColor("#FB9203")])),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(
+              height: 8.0,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              child: const GradientText(
+                "জল এবং গ্যাস সংযোগ পরিষেবা",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Divider(
-                thickness: 1,
-                indent: 9.0,
-                color: Colors.grey,
-                endIndent: 12.0,
-              ),
+            ),
+            Divider(
+              thickness: 1,
+              indent: 9.0,
+              color: Colors.grey,
+              endIndent: 12.0,
+            ),
 
-             CustomTextField(label: 'বর্তমান পানি সংযোগ এর ঠিকানা',hint: 'Jannatul Ferdous', onChange: (){}),
+            CustomTextField(
+                label: 'বর্তমান পানি সংযোগ এর ঠিকানা',
+                hint: 'Jannatul Ferdous',
+                onChange: () {}),
 
-          // SizedBox(
-          //   height: 30.0,
-          // ),
-              CustomTextField(label: 'হোল্ডিং নং', hint: '', onChange: (){}),
+            // SizedBox(
+            //   height: 30.0,
+            // ),
+            CustomTextField(label: 'হোল্ডিং নং', hint: '', onChange: () {}),
 
-
-                   CustomDropdown(
-                       label: "নির্বাচন করুন",
-                       items: _items,
-                       hint: "নির্বাচন করুন",
-                       require: true,
-                       onChange: (String? value) {
-                         debugPrint("$value");
-                       }),
-                 ],
-               ),
-
-
-
-
-
-          ),
-
+            CustomDropdown(
+                label: "নির্বাচন করুন",
+                items: _items,
+                hint: "নির্বাচন করুন",
+                require: true,
+                onChange: (String? value) {
+                  debugPrint("$value");
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
